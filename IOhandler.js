ced93343f68794d95aef4d9b1f4c587a3f420df6
Norvin <chnorvin@gmail.com>
2023-10-18 17:47:58 -0700

    Lab completion

--- IOhandler.js
+++ IOhandler.js
@@ -10,55 +10,57 @@
 
 const { rejects } = require("assert");
 
-const unzipper = require("unzipper"),
-  fs = require("fs"),
-  PNG = require("pngjs").PNG,
-  path = require("path");
+const fs = require("fs");
+const PNG = require("pngjs").PNG;
+const path = require("path");
+const AdmZip = require("adm-zip");
 
 /**
  * Description: decompress file from given pathIn, write to given pathOut
  *
  * @param {string} pathIn
  * @param {string} pathOut
- * @return {promise}
+ * @return {Promise}
  */
 const unzip = (pathIn, pathOut) => {
   return new Promise((resolve, reject) => {
-    fs.createReadStream(pathIn)
-      .pipe(unzipper.Extract({ path: pathOut }))
-      .on('close', () => {
-        resolve("Extraction operation complete");
-      })
-      .on('error', (error) => {
-        reject(error);
-      });
+    const zip = new AdmZip(pathIn);
+
+    try {
+      zip.extractAllTo(pathOut, true);
+      resolve("Extraction operation complete");
+    } catch (error) {
+      reject(`Error extracting zip file: ${error}`);
+    }
   });
 };
 
-
 /**
- * Description: read all the png files from given directory and return Promise containing array of each png file path
+ * Description: Read all the PNG files from the given directory and return a Promise containing an array of each PNG file path
  *
- * @param {string} path
- * @return {promise}
+ * @param {string} dir
+ * @return {Promise}
  */
-
 const readDir = (dir) => {
   return new Promise((resolve, reject) => {
     fs.readdir(dir, (err, files) => {
       if (err) reject(err);
-      else resolve(files.filter(file => path.extname(file).toLowerCase() === '.png').map(file => path.join(dir, file)));
+      else
+        resolve(
+          files
+            .filter((file) => path.extname(file).toLowerCase() === ".png")
+            .map((file) => path.join(dir, file))
+        );
     });
   });
 };
 
 /**
- * Description: Read in png file by given pathIn,
- * convert to grayscale and write to given pathOut
+ * Description: Read in a PNG file by given pathIn, convert to grayscale, and write to the given pathOut
  *
- * @param {string} filePath
- * @param {string} pathProcessed
- * @return {promise}
+ * @param {string} pathIn
+ * @param {string} pathOut
+ * @return {Promise}
  */
 const grayScale = (pathIn, pathOut) => {
   return new Promise((resolve, reject) => {
